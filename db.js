9c442e1194dbd2b4556ea98ec26fd2f151a01ea8
Nitul Chandra Dutta <Nituldutta543@gmail.com>
2026-04-08 13:18:52 +0530

    Changes for Vercel

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const mongoURL = process.env.MONGO_URL;
 
-const connectToMongo = () => {
-    mongoose.connect(mongoURL).then(() => {
+const connectToMongo = async () => {
+    await mongoose.connect(mongoURL).then(() => {
         console.log("Connected to Mongo Successfully");
     }).catch((err) => {
         console.log("Error connecting to Mongo: ", err);
--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 require('dotenv').config();
 const connectToMongo = require("./db");
 const express = require("express");
-connectToMongo();
 var cors = require('cors');
+
+//Connect to MongoDB
+connectToMongo();
 
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+//Root Route
+app.get("/", (req, res) => {
+    res.send("Welcome to NoteNext Backend");
+});
 
 //Routes
 app.use('/api/auth', require('./routes/auth'));
@@ -18,3 +25,5 @@ app.use('/api/notes', require('./routes/notes'));
 app.listen(port, () => {
   console.log(`NoteNext Backend listening at http://localhost:${port}`);
 });
+
+module.exports = app;
